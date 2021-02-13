d39e5d773a0f30514fc6b5327b973320e4305709
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-02-13 10:18:03 +0700

    Initial Commit

---
+++ UnitTestCourse/UnitTestCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestCourseApp.swift
+//  UnitTestCourse
+//
+//  Created by Runyasak Chaengnaimuang on 13/2/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
